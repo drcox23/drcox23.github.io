74d11671e553c6dfac44c2df8d0b1c384b91cecf
doug <drcox23@gmail.com>
2019-02-12 22:51:14 -1000

    more css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("Aloha!")
+console.log('Aloha!');
+
+projLink = url => {
+  window.location.pathname = url;
+};
+
+let projects = document.getElementsByClassName('proj-space');
+
+// projects.addEventListener('click', projLink('./projects/grok-mobile.html'), false);
